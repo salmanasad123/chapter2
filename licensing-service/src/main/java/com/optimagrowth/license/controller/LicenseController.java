049bfda0570dda893421270269d6d9228c499818
salmanasad <sasad@nisum.com>
2022-03-07 11:12:13 +0500

    Initial commit

---
+++ licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -0,0 +1,51 @@
+package com.optimagrowth.license.controller;
+
+import com.optimagrowth.license.model.License;
+import com.optimagrowth.license.service.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController   // Tells Spring Boot that this is a REST- based service and it will automatically serialize/deserialize service requests/responses via JSON.
+@RequestMapping(value = "v1/organization/{organizationId}/license")
+public class LicenseController {
+
+    @Autowired
+    private LicenseService licenseService;
+
+    @RequestMapping(value = "/{licenseId}", method = RequestMethod.GET)
+    public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId,
+                                              @PathVariable("licenseId") String licenseId) {
+
+        License license = licenseService.getLicense(licenseId, organizationId);
+        license.add(
+                linkTo(methodOn(LicenseController.class).getLicense(organizationId, license.getLicenseId())).withSelfRel(),
+                linkTo(methodOn(LicenseController.class).createLicense(license)).withRel("createLicense"),
+                linkTo(methodOn(LicenseController.class).updateLicense(license)).withRel("updateLicense"),
+                linkTo(methodOn(LicenseController.class).deleteLicense(license.getLicenseId())).withRel("deleteLicense")
+        );
+
+        return ResponseEntity.ok(license);
+    }
+
+    @PutMapping
+    public ResponseEntity<License> updateLicense(@RequestBody License request) {
+        License license = licenseService.updateLicense(request);
+        return ResponseEntity.ok(license);
+    }
+
+    @PostMapping
+    public ResponseEntity<License> createLicense(@RequestBody License request) {
+        License license = licenseService.createLicense(request);
+        return ResponseEntity.ok(license);
+    }
+
+    @DeleteMapping(value = "/{licenseId}")
+    public ResponseEntity<String> deleteLicense(@PathVariable("licenseId") String licenseId) {
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId));
+    }
+}
+
